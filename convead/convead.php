7d777fc9d7946915c4b26378e7df43c4f097b40c
rostber <vova@convead.com>
2015-10-12 17:56:21 +0300

    Init version 1.0

---
+++ convead/convead.php
@@ -0,0 +1,202 @@
+<?php
+
+if (!defined('_PS_VERSION_')) exit;
+
+class Convead extends Module
+{ 
+  public function __construct()
+  {
+    $this->name = 'convead';
+    $this->tab = 'analytics_stats';
+    $this->version = '1.0';
+    $this->author = 'Rostber';
+    $this->displayName = $this->l('Convead');
+    $this->module_key = '95857606d6e384f5992eeb0771f7a3a5';
+    
+    parent::__construct();
+    
+    if ($this->id AND !Configuration::get('APP_KEY')) $this->warning = $this->l('You have not yet set your Convead APP_KEY');
+    $this->description = $this->l('Integrate Convead script into your shop');
+    $this->confirmUninstall = $this->l('Are you sure you want to delete your details ?');
+  }
+
+  function install()
+  {
+    if (!parent::install() ||
+        !$this->registerHook('header') ||
+        !$this->registerHook('orderConfirmation') ||
+        !$this->registerHook('cart') ||
+        !$this->registerHook('shoppingCart')
+      ) return false;
+    return true;
+  }
+  
+  function uninstall()
+  {
+    if (!Configuration::deleteByName('APP_KEY') || !parent::uninstall()) return false;
+    return true;
+  }
+  
+  public function getContent()
+  {
+    $output = '<h2>Convead Analytics</h2>';
+    if (Tools::isSubmit('submitConvead') AND ($app_key = Tools::getValue('APP_KEY')))
+    {
+      Configuration::updateValue('APP_KEY', $app_key);
+      $output .= '
+      <div class="conf confirm">
+        <img src="../img/admin/ok.gif" alt="" title="" />
+        '.$this->l('Settings updated').'
+      </div>';
+    }
+    return $output.$this->displayForm();
+  }
+
+  public function displayForm()
+  {
+    $output = '';
+
+    $error_array = array();
+    if (!function_exists('curl_exec')) $error_array[] = $this->l('Disabled expansion curl');
+    if (!extension_loaded('mbstring')) $error_array[] = $this->l('Disabled expansion mbstring');
+    if (count($error_array) > 0) $output .= '
+      <div class="error">
+        <span class="required">'.implode('<br />', $error_array).'</span><br />
+        <b>'.$this->l('Contact support hosting').'</b>
+      </div>
+    ';
+
+    $output .= '
+    <form action="'.Tools::safeOutput($_SERVER['REQUEST_URI']).'" method="post">
+      <fieldset>
+        <legend><img src="../img/admin/cog.gif" alt="" class="middle" />'.$this->l('Settings').'</legend>
+        <label>'.$this->l('app_key').'</label>
+        <div class="margin-form">
+          <input type="text" name="APP_KEY" value="'.Tools::safeOutput(Tools::getValue('APP_KEY', Configuration::get('APP_KEY'))).'" />
+        </div>
+        <input type="submit" name="submitConvead" value="'.$this->l('Save').'" class="button" />
+      </fieldset>
+    </form>';
+    return $output;
+  }
+  
+  function hookHeader($params)
+  {
+    $app_key = Configuration::get('APP_KEY');
+
+    if (empty($app_key)) return false;
+
+    $this->context->smarty->assign('app_key', $app_key);
+
+    if ($this->context->customer->isLogged())
+    {
+      $this->context->smarty->assign('customer', $this->context->customer);
+    }
+    else $this->context->smarty->assign('customer', false);
+
+    if (Dispatcher::getInstance()->getController() == 'product' && $product_id = Tools::getValue('id_product'))
+    {
+      $product = new Product($product_id, true, $this->context->language->id, $this->context->shop->id);
+      $this->context->smarty->assign('is_product_page', true);
+      $this->context->smarty->assign('product_id', $product_id);
+      $this->context->smarty->assign('product_name', $product->name);
+      $this->context->smarty->assign('category_id', $product->id_category_default);
+    }
+    else $this->context->smarty->assign('is_product_page', false);
+
+    return $this->display(__FILE__, 'header.tpl');
+  }
+  
+  function hookFooter($params)
+  {
+    // for retrocompatibility
+    if (!$this->isRegisteredInHook('header'))
+      $this->registerHook('header');
+    return ;
+  }
+
+  function hookShoppingCart($params)
+  {
+    return $this->_updateCart($params);
+  }
+
+  function hookCart($params)
+  {
+    return $this->_updateCart($params);
+  }
+
+  function hookOrderConfirmation($params)
+  {
+    if (!($convead = $this->_includeApi())) return;
+
+    $parameters = Configuration::getMultiple(array('PS_LANG_DEFAULT'));
+    
+    $order = $params['objOrder'];
+    if (Validate::isLoadedObject($order))
+    {
+      $conversion_rate = 1;
+      if ($order->id_currency != Configuration::get('PS_CURRENCY_DEFAULT'))
+      {
+        $currency = new Currency(intval($order->id_currency));
+        $conversion_rate = floatval($currency->conversion_rate);
+      }
+
+      $products = $order->getProducts();
+      $products_array = array();
+      foreach ($products AS $product)
+      {
+        $products_array[] = array(
+          'product_id' => addslashes($product['product_id']),
+          'qnt' => addslashes(intval($product['product_quantity'])),
+          //'product_name' => addslashes($product['product_name']),
+          'price' => Tools::ps_round(floatval($product['product_price_wt']) / floatval($conversion_rate), 2)
+        );
+      }
+
+      $total = Tools::ps_round(floatval($order->total_paid) / floatval($conversion_rate), 2);
+      //$shipping = Tools::ps_round(floatval($order->total_shipping) / floatval($conversion_rate), 2);
+
+      $convead->eventOrder(intval($order->id), $total, $products_array);
+    }
+
+    return;
+  }
+
+  function _updateCart($params)
+  {
+    if (!($convead = $this->_includeApi()) || !isset($params['cart'])) return;
+
+    $c_products_cart = array();
+    $products_cart = $params['cart']->getProducts(true);
+    foreach($products_cart as $product)
+    {
+      $c_products_cart[] = array('product_id' => $product['id_product'], 'qnt' => $product['cart_quantity'], 'price' => $product['price']);
+    }
+    $convead->eventUpdateCart($c_products_cart);
+  }
+
+  function _includeApi()
+  {
+    $app_key = Configuration::get('APP_KEY');
+
+    if (empty($app_key)) return false;
+
+    include_once('api/ConveadTracker.php');
+
+    if ($this->context->customer->isLogged())
+    {
+      $user_id = $this->context->customer->id;
+      $guest_uid = false;
+    }
+    else 
+    {
+      if (empty($_COOKIE['convead_guest_uid'])) return false;
+      $guest_uid = $_COOKIE['convead_guest_uid'];
+    }
+
+    $convead = new ConveadTracker($app_key, $_SERVER['SERVER_NAME'], $guest_uid, (isset($user_id) ? $user_id : false), (isset($visitor_info) ? $visitor_info : false));
+    
+    return $convead;
+  }
+
+}
---
+++ convead/header.tpl
@@ -0,0 +1,33 @@
+<!-- Convead Widget -->
+{literal}
+<script>
+window.ConveadSettings = {
+    {/literal}{if $customer}{literal}
+    visitor_uid: '{/literal}{$customer->id}{literal}',
+    visitor_info: {
+        first_name: '{/literal}{$customer->firstname}{literal}',
+        last_name: '{/literal}{$customer->lastname}{literal}',
+        email: '{/literal}{$customer->email}{literal}'
+    },
+    {/literal}{/if}{literal}
+    {/literal}{if $is_product_page}{literal}
+    onready: function() {
+        convead('event', 'view_product', {
+            product_id: '{/literal}{$product_name}{literal}',
+            product_name: '{/literal}{$product_name}{literal}',
+            category_id: '{/literal}{$category_id}{literal}',
+            product_url: window.location.href
+        });
+    },
+    {/literal}{/if}{literal}
+    app_key: "{/literal}{$app_key}{literal}"
+
+    /* For more information on widget configuration please see:
+       http://convead.ru/help/kak-nastroit-sobytiya-vizitov-prosmotrov-tovarov-napolneniya-korzin-i-pokupok-dlya-vashego-sayta
+    */
+};
+
+(function(w,d,c){w[c]=w[c]||function(){(w[c].q=w[c].q||[]).push(arguments)};var ts = (+new Date()/86400000|0)*86400;var s = d.createElement('script');s.type = 'text/javascript';s.async = true;s.src = 'https://tracker.convead.io/widgets/'+ts+'/widget-{/literal}{$app_key}{literal}.js';var x = d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);})(window,document,'convead');
+</script>
+{/literal}
+<!-- /Convead Widget -->
